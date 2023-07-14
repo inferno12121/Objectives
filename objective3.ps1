10382b18b1ff5e9f69744cb921a0be7074898be0
Abdul Hubbali <98153325+inferno12121@users.noreply.github.com>
2023-07-15 00:05:31 +0530

    Add files via upload

---
+++ objective3.ps1
@@ -0,0 +1,21 @@
+﻿$aValue = Read-Host -Prompt "Enter the value for 'a'"
+$bValue = Read-Host -Prompt "Enter the value for 'b'"
+$cValue = Read-Host -Prompt "Enter the value for 'c'"
+$dValue = Read-Host -Prompt "Enter the value for 'd'"
+
+$object1 = @{ "a" = @{ "b" = @{ "c" = $cValue } } }
+$object1["a"]["b"]["c"] = $dValue
+
+$key1 = Read-Host -Prompt "Enter the key"
+
+$value = $object1
+$props = $key1 -split '/'
+foreach ($prop in $props) {
+    $value = $value.$prop
+    if ($value -eq $null) {
+        $value = $null
+        break
+    }
+}
+
+Write-Host "Value 1: $value"
