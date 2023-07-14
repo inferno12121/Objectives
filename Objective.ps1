9e54783e63859c0c3bb479d7099ef17e50a66e9f
inferno12121 <98153325+inferno12121@users.noreply.github.com>
2023-07-14 23:58:09 +0530

    Add files via upload
    Objectives1,2 & 3

---
+++ Objective.ps1
@@ -0,0 +1,20 @@
+﻿Connect-AzAccount
+Select-AzSubscription #subscriptionID
+$RG = "testresourcegroup"
+$loc = "eastus"
+
+New-AzResourceGroup -Name $RG -location $loc
+
+$appServicePlanName = "myAppServicePlan"
+New-AzAppServicePlan -ResourceGroupName  $RG -Name $appServicePlanName -location $loc -Tier "Free"
+
+$webAppName = "myWebApp" + [DateTime]::Now.Ticks.ToString()
+New-AzWebApp -ResourceGroupName  $RG -Name $webAppName -location $loc -AppServicePlan $appServicePlanName
+
+$sqlServerName = "my-sql-server-" + [DateTime]::Now.Ticks.ToString()
+$databaseName = "myDatabase"
+$adminLogin = "myAdminLogin"
+$adminPassword = "Comas5738q029#ord"
+
+New-AzSqlServer -ResourceGroupName  $RG -ServerName $sqlServerName -location $loc -SqlAdministratorCredentials (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminLogin, (ConvertTo-SecureString -String $adminPassword -AsPlainText -Force))
+New-AzSqlDatabase -ResourceGroupName  $RG -ServerName $sqlServerName -DatabaseName $databaseName -Edition "Basic"
---
+++ objective2.ps1
@@ -0,0 +1,13 @@
+﻿Connect-AzAccount # connect to subscription 
+Select-AzSubscription SubscriptionID # enter the ID
+$resourceGroups = Get-AzResourceGroup # get all RGs
+foreach ($resourceGroup in $resourceGroups) {
+    $resourceGroupName = $resourceGroup.ResourceGroupName
+    $vms = Get-AzVm -ResourceGroupName $resourceGroupName
+    foreach ($vm in $vms) {
+        $vmName = $vm.Name
+        $metadata = $vm.OSProfile.Metadata
+        Write-Host "Metadata for VM: $vmName in Resource Group: $resourceGroupName"
+        $metadata
+    }
+}
